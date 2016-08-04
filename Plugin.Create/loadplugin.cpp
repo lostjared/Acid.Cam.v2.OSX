8eebd1b2089fcdae7aaaf55c5c1d6f9e5f858cc6
lostjared <jaredbruni@gmail.com>
2016-08-04 16:20:25 -0700

    formatting..

--- Plugin.Create/loadplugin.cpp
+++ Plugin.Create/loadplugin.cpp
@@ -1,6 +1,6 @@
-/* Plugin Loading test source file 
-   written by Jared Bruni
-*/
+/* Plugin Loading test source file
+ written by Jared Bruni
+ */
 
 
 
@@ -29,7 +29,7 @@ void testplugin(int iterations) {
 }
 
 int main(int argc, char **argv) {
-
+    
     if(argc != 3) {
         std::cerr << "Use:\n" << argv[0] << " plugin iterations\n";
         exit(1);
@@ -41,39 +41,39 @@ int main(int argc, char **argv) {
         exit(1);
     }
     
-	void *library;
-	library = dlopen(argv[1], RTLD_LAZY);
-	
-	if(library == NULL) {
-		std::cerr << "Error could not open: " << argv[1] << "\n";
-		exit(1);
-	}
-	
-	void *addr;
-
-	// load the plugin function to process pixels
-	addr = dlsym(library, "pixel");
-	pix = reinterpret_cast<pixel>(addr);
-	const char *error;
-	error = dlerror();
-	if(error) {
-		std::cerr << "Could not load pixel: " << error << "\n";
-		exit(1);
-	}
-
-	void *dr = dlsym(library,"drawn");
-	drawn_ = reinterpret_cast<drawn>(dr);
-	error = dlerror();
-	if(error) {
-		std::cerr << "Error could not load drawn function: " << error << "\n";
-		exit(1);
-	}
-
-	// test plugin:
+    void *library;
+    library = dlopen(argv[1], RTLD_LAZY);
+    
+    if(library == NULL) {
+        std::cerr << "Error could not open: " << argv[1] << "\n";
+        exit(1);
+    }
+    
+    void *addr;
+    
+    // load the plugin function to process pixels
+    addr = dlsym(library, "pixel");
+    pix = reinterpret_cast<pixel>(addr);
+    const char *error;
+    error = dlerror();
+    if(error) {
+        std::cerr << "Could not load pixel: " << error << "\n";
+        exit(1);
+    }
+    
+    void *dr = dlsym(library,"drawn");
+    drawn_ = reinterpret_cast<drawn>(dr);
+    error = dlerror();
+    if(error) {
+        std::cerr << "Error could not load drawn function: " << error << "\n";
+        exit(1);
+    }
+    
+    // test plugin:
     testplugin(iter);
-
-	dlclose(library);
-        std::cout << "Plugin executed successfully..\n";
-	return 0;
-	
+    
+    dlclose(library);
+    std::cout << "Plugin executed successfully..\n";
+    return 0;
+    
 }
