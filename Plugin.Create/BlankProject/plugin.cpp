c857c76b340220ade2a969f0cfdd6f11fb4cdcfe
lostjared <jaredbruni@gmail.com>
2016-08-06 00:50:04 -0700

    added blank plugin project

---
+++ Plugin.Create/BlankProject/plugin.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+extern "C" void pixel(int x, int y, unsigned char *pix);
+extern "C" void drawn();
+
+// pixel operation here, called once for every pixel each frame
+void pixel(int x, int y, unsigned char *pix) {
+       
+}
+// called when frame is done drawing
+void drawn() {
+
+}
+
