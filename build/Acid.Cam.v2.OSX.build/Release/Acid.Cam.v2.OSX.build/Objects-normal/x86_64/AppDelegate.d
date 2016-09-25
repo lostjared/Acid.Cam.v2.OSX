3f27f0aa337aa56a8fd1245dd3d5c0d142a3cfad
lostjared <jaredbruni@gmail.com>
2016-09-25 15:03:03 -0700

    removed build

--- build/Acid.Cam.v2.OSX.build/Release/Acid.Cam.v2.OSX.build/Objects-normal/x86_64/AppDelegate.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Volumes/LostSideDrive-2/Source/AcidCam.August.2016/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/AppDelegate.m \
-  /Volumes/LostSideDrive-2/Source/AcidCam.August.2016/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/AppDelegate.h
