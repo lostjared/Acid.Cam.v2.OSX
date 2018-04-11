6d1158db4df825443a76e6897fae24bc2a54aac8
lostjared <jaredbruni@gmail.com>
2018-04-11 16:30:14 -0700

    added new filter (last mirror one for a good while)

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -298,6 +298,7 @@ namespace ac {
     void InterReverse(cv::Mat &frame);
     void InterMirror(cv::Mat &frame);
     void InterFullMirror(cv::Mat &frame);
+    void MirrorRGB(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
