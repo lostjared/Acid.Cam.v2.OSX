eb409ee8a778efbc026724fe104612b69eed07d0
lostjared <jaredbruni@gmail.com>
2018-04-11 12:26:12 -0700

    added another inter mirror filter

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -297,6 +297,7 @@ namespace ac {
     void TrailsNegate(cv::Mat &frame);
     void InterReverse(cv::Mat &frame);
     void InterMirror(cv::Mat &frame);
+    void InterFullMirror(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
