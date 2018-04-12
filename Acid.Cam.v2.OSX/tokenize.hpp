519127c5eb036959e3053bb92df8b542ad516a42
lostjared <jaredbruni@gmail.com>
2018-04-12 16:18:53 -0700

    updated code now it won't add extra spaces

--- Acid.Cam.v2.OSX/tokenize.hpp
+++ Acid.Cam.v2.OSX/tokenize.hpp
@@ -52,7 +52,9 @@ namespace token {
         size_t i = find<type>(0,source,delim),z=0;
         size_t lenz = len<type>(source), dlen = len<type>(delim);
         while ( i != 0 && i < lenz && z < lenz ) {
-            v.push_back(substr(source,z,i));
+            std::string s = substr(source,z,i);
+            if(s.length() > 0 && s != " ")
+            	v.push_back(s);
             z = i+dlen;
             i = find<type>(i+1, source, delim);
         }
