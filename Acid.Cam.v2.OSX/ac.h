6e843fff3a148831017de5123b1cb512978c9963
lostjared <jaredbruni@gmail.com>
2018-05-05 09:16:55 -0700

    new filter / updated Grid

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -329,6 +329,7 @@ namespace ac {
     void SmoothTrails(cv::Mat &frame);
     void GridFilter8x(cv::Mat &frame);
     void GridFilter16x(cv::Mat &frame);
+    void GridFilter8xBlend(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
