bc64887ce635aae24187c0f74f62c2541ec6063a
lostjared <jaredbruni@gmail.com>
2018-05-10 16:23:19 -0700

    new filter

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -337,6 +337,7 @@ namespace ac {
     void SurroundPixelXor(cv::Mat &frame);
     void Darken(cv::Mat &frame);
     void WeakBlend(cv::Mat &frame);
+    void AverageVertical(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
