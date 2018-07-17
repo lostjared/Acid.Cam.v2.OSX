c832481d369621ad9fe2e8612a8b1661eab7d345
lostjared <jaredbruni@gmail.com>
2018-07-17 05:34:10 -0700

    added ac-filter8.cpp

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -487,7 +487,8 @@ namespace ac {
     void ParticleFast(cv::Mat &frame);
     void RandomXorOpposite(cv::Mat &frame);
     void StrobeTransform(cv::Mat &frame);
-    void InitBlend(cv::Mat &frame);    
+    void InitBlend(cv::Mat &frame);
+    void TestFilter10z(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
