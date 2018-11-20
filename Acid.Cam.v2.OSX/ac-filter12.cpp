79fb85584880624b47dcfd38856d85fc2d9388e6
lostjared <jaredbruni@gmail.com>
2018-11-19 21:57:15 -0800

    fixed bug was supposed to be rows not cols

--- Acid.Cam.v2.OSX/ac-filter12.cpp
+++ Acid.Cam.v2.OSX/ac-filter12.cpp
@@ -334,7 +334,7 @@ void ac::DivideAndIncW(cv::Mat &frame) {
     unsigned int row_x = frame.cols/255;
     unsigned int row_y = frame.rows/255;
     for(int i = 0; i < frame.cols; ++i) {
-        for(int z = 0; z < frame.cols; ++z) {
+        for(int z = 0; z < frame.rows; ++z) {
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             pixel[0] += x;
             pixel[1] += y;
