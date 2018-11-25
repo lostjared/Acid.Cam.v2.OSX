94b6ad07f4e5198be5d5ca3643ee1d76955c6ac1
lostjared <jaredbruni@gmail.com>
2018-11-24 17:35:13 -0800

    was using wrong name

--- Acid.Cam.v2.OSX/ac-filter12.cpp
+++ Acid.Cam.v2.OSX/ac-filter12.cpp
@@ -658,7 +658,7 @@ void ac::GammaIncDecIncrease(cv::Mat &frame) {
 }
 
 void ac::RandomSubFilter(cv::Mat &frame) {
-    if(subfilter == -1 || ac::draw_strings[subfilter] == "ShuffleSub")
+    if(subfilter == -1 || ac::draw_strings[subfilter] == "RandomSubFilter")
         return;
     static int index = 0;
     static std::vector<std::string> vSub { "Bitwise_XOR_AlphaSubFilter", "AlphaBlendSubFilter", "GradientSubFilterXor", "XorBlend_SubFilter","EnergizeSubFilter","PixelatedSubFilterSort","FilteredDifferenceSubFilter","ExpandSquareSubFilter","MirrorEnergizeSubFilter", "InterRGB_SubFilter", "InterSmoothSubFilter", "StoredFramesAlphaBlend_SubFilter", "BlendSubFilter", "BlendAlphaSubFilter", "Blend_AlphaSubFilter", "FrameMedianBlendSubFilter", "FrameBlurSubFilter","SubFilterMedianBlend", "DarkCollectionSubFilter", "FlipMedianSubFilter", "FlipMirrorSubFilter", "BlendCombinedValueSubFilter","CollectionXorSourceSubFilter","BlendReverseSubFilter","SmoothBlendReverseSubFilter","MedianBlendBufferSubFilter","RGBBlendSubFilter","XorOppositeSubFilter", "BlendSmoothSubFilter", "BlurSmoothSubFilter", "BlurFlipSubFilter", "MedianBlendSubFilterEx", "ShiftFrameSmoothSubFilter", "ShiftFrameStaticXorSubFilter"};
