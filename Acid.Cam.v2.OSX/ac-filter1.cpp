89be8c5ca6d8f5bc4904020592e78fbbe592af51
lostjared <jaredbruni@gmail.com>
2019-02-25 06:48:32 -0800

    Updated plist file with camera description for access on mojave

--- Acid.Cam.v2.OSX/ac-filter1.cpp
+++ Acid.Cam.v2.OSX/ac-filter1.cpp
@@ -45,13 +45,13 @@
 // Acid Cam namespace
 namespace ac {
 #if defined(__APPLE__)
-    const std::string version="2.18.1 (macOS)";
+    const std::string version="2.18.2 (macOS)";
 #elif defined(__linux__)
-    const std::string version="2.18.1 (Linux)";
+    const std::string version="2.18.2 (Linux)";
 #elif defined(_WIN32)
-    const std::string version="2.18.1 (Windows)";
+    const std::string version="2.18.2 (Windows)";
 #else
-    const std::string version="2.18.1 (Generic)";
+    const std::string version="2.18.2 (Generic)";
 #endif
     // variables
     int swapColor_r = 0, swapColor_g = 0, swapColor_b = 0;
