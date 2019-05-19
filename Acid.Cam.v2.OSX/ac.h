85bf9e72d22a9ea4607e80ee0a4857ca50496b03
Jared Bruni <jaredbruni@gmail.com>
2019-05-19 13:24:59 -0700

    forgot to include ocl.hpp

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -46,9 +46,10 @@
 #include<opencv2/videoio.hpp>
 #include<opencv2/imgproc.hpp>
 #include<opencv2/highgui.hpp>
-#include <opencv2/core/ocl.hpp>
+#include<opencv2/core/ocl.hpp>
 #else
 #include<opencv2/opencv.hpp>
+#include<opencv2/core/cocl.hpp>
 #endif
 #include<iostream>
 #include<fstream>
