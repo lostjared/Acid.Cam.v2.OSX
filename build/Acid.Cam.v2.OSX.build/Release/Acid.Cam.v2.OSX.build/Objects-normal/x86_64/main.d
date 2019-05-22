1f555bb456d081bceb5213ae14f88ac5d70845c0
Jared Bruni <jaredbruni@gmail.com>
2019-05-22 12:47:10 -0700

    removed unneeded files and fixed opencv header file

--- build/Acid.Cam.v2.OSX.build/Release/Acid.Cam.v2.OSX.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX-Prefix.pch \
-  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/main.m
--- build/Acid.Cam.v2.OSX.build/Release/Acid.Cam.v2.OSX.build/Objects-normal/x86_64/tokenize.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX-Prefix.pch \
-  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/tokenize.cpp \
-  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/tokenize.hpp
