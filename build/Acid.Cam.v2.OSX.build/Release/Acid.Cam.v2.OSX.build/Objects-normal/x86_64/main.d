d12cdc258b33ef5bb62279d7bca9a9b3ca0b6b2f
Jared Bruni <jaredbruni@gmail.com>
2019-05-22 12:46:16 -0700

    fixed warning in OpenCV header file

---
+++ build/Acid.Cam.v2.OSX.build/Release/Acid.Cam.v2.OSX.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX-Prefix.pch \
+  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/main.m
---
+++ build/SharedPrecompiledHeaders/SharedPrecompiledHeaders/2826195232711574776/Acid.Cam.v2.OSX-Prefix.pch.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jared/Source/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX/Acid.Cam.v2.OSX-Prefix.pch
