e9d52fba84d531c9dabe0e6e5247709db20dafed
Jared Bruni <jaredbruni@gmail.com>
2019-06-29 09:50:38 -0700

    new one

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -1418,6 +1418,7 @@ namespace ac {
     void ColorImageMastrixFadeFast(cv::Mat &frame);
     void ColorImageMatrixFadeDirection(cv::Mat &frame);
     void ColorImageMatrixFadeDirectionBlend(cv::Mat &frame);
+    void ColorMatrixCollection(cv::Mat &frame);
     // No filter (do nothing)
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
