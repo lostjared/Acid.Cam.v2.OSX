8ebff420f2f3db0376d5ac9c8413fb32561cbc93
Jared Bruni <jaredbruni@gmail.com>
2019-07-14 12:08:51 -0700

    changed it back

--- Acid.Cam.v2.OSX/ac-filter25.cpp
+++ Acid.Cam.v2.OSX/ac-filter25.cpp
@@ -779,17 +779,17 @@ void ac::StretchCollection(cv::Mat &frame) {
     AddInvert(frame);
 }
 
-void ac::SlitScanGlitch(cv::Mat &frame) {
+void ac::SlitScanStretch(cv::Mat &frame) {
     static MatrixCollection<720> collection;
     cv::Mat resized;
     ac_resize(frame, resized, cv::Size(1280, 720));
     collection.shiftFrames(resized);
     for(int z = 0; z < resized.rows; ++z) {
-        int max = 5+(rand()%20);
+        int max = 5+(rand()%50);
         for(int q = 0; q < max && z+q < resized.rows; ++q) {
             for(int i = 0; i < resized.cols; ++i) {
-                cv::Vec3b value = collection.frames[z].at<cv::Vec3b>(z+q, i);
                 cv::Vec3b &pixel = resized.at<cv::Vec3b>(z+q, i);
+                cv::Vec3b value = collection.frames[z+q].at<cv::Vec3b>(z+q, i);
                 pixel = value;
             }
         }
