a766e68c6a6082da58b041a4a4f7ec7bc3c87ad7
Jared Bruni <jaredbruni@gmail.com>
2019-08-16 09:40:04 -0700

    Updated filters

--- Acid.Cam.v2.OSX/ac-filter28.cpp
+++ Acid.Cam.v2.OSX/ac-filter28.cpp
@@ -457,6 +457,22 @@ void ac::LineMedianBlend(cv::Mat &frame) {
 }
 
 void ac::PerfectMedianBlend(cv::Mat &frame) {
-    GradientRandom(frame);
+    static int index = 0;
+    static unsigned char val = 0;
+    static double alpha = 1.0;
+    static int dir = 1;
+    int inc = (frame.rows/255)+1;
+    for(int i = 0; i < frame.cols; ++i) {
+        val = 1;
+        for(int z = 0; z < frame.rows; ++z) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            pixel[index] = static_cast<unsigned char>((alpha * pixel[index]) + ((1-alpha) * val));
+            if((z%inc) == 0)
+                ++val;}
+    }
+    ++index;
+    if(index > 2)
+        index = 0;
+    AlphaMovementMaxMin(alpha, dir, 0.1, 1.0, 0.1);
     MedianBlendMultiThread(frame);
 }
