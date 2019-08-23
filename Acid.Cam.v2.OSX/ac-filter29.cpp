15ffffe82bdfc61afaacbd386e257134baf61e4a
Jared Bruni <jaredbruni@gmail.com>
2019-08-23 09:38:59 -0700

    added new source file

---
+++ Acid.Cam.v2.OSX/ac-filter29.cpp
@@ -0,0 +1,112 @@
+
+
+/*
+ * Software written by Jared Bruni https://github.com/lostjared
+ 
+ This software is dedicated to all the people that experience mental illness.
+ 
+ Website: http://lostsidedead.com
+ YouTube: http://youtube.com/LostSideDead
+ Instagram: http://instagram.com/lostsidedead
+ Twitter: http://twitter.com/jaredbruni
+ Facebook: http://facebook.com/LostSideDead0x
+ 
+ You can use this program free of charge and redistrubute it online as long
+ as you do not charge anything for this program. This program is meant to be
+ 100% free.
+ 
+ BSD 2-Clause License
+ 
+ Copyright (c) 2019, Jared Bruni
+ All rights reserved.
+ 
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ 
+ * Redistributions of source code must retain the above copyright notice, this
+ list of conditions and the following disclaimer.
+ 
+ * Redistributions in binary form must reproduce the above copyright notice,
+ this list of conditions and the following disclaimer in the documentation
+ and/or other materials provided with the distribution.
+ 
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ 
+ */
+
+/*
+ //Basic Multithreaded Filter
+ auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
+ for(int z = offset; z <  offset+size; ++z) {
+ for(int i = 0; i < cols; ++i) {
+ }
+ }
+ };
+ UseMultipleThreads(frame, getThreadCount(), callback);
+ AddInvert(frame);
+ 
+ */
+
+#include"ac.h"
+
+void ac::ImageMirror_DownSubFilter(cv::Mat &frame) {
+    if(blend_set == false || subfilter == -1 || draw_strings[subfilter] == "ImageMirror_DownSubFilter")
+        return;
+    cv::Mat reimage;
+    ac_resize(blend_image, reimage, frame.size());
+    CallFilter(subfilter, reimage);
+    cv::Mat copy1 = reimage.clone();
+    int halfway = (frame.rows/2);
+    for(int i = 0; i < frame.cols; ++i) {
+        for(int z = 0; z < halfway; ++z) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Vec3b pix = copy1.at<cv::Vec3b>(frame.rows-z-1, i);
+            ASSERT(frame.rows-z-1 > 0);
+            for(int j = 0; j < 3; ++j) {
+                pixel[j] = static_cast<unsigned char>((0.5 * pixel[j]) + (0.5 * pix[j]));
+            }
+        }
+    }
+    for(int i = 0; i < frame.cols; ++i) {
+        for(int z = halfway; z < frame.rows; ++z) {
+            cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+            cv::Vec3b pix = copy1.at<cv::Vec3b>(z, i);
+            ASSERT(frame.rows-z-1 > 0);
+            for(int j = 0; j < 3; ++j) {
+                pixel[j] = static_cast<unsigned char>((0.5 * pixel[j]) + (0.5 * pix[j]));
+            }
+        }
+    }
+    AddInvert(frame);
+}
+
+void ac::ImageMirrorKaleidoscope(cv::Mat &frame) {
+    if(blend_set == false)
+        return;
+    pushSubFilter(getFilterByName("ColorPulseIncrease"));
+    ImageMirror_LeftSubFilter(frame);
+    popSubFilter();
+    pushSubFilter(getFilterByName("ColorTransition"));
+    ImageMirror_UpSubFilter(frame);
+    popSubFilter();
+    MirrorLeftBottomToTop(frame);
+    MedianBlendMultiThread(frame);
+}
+
+void ac::ExactImageSubFilter(cv::Mat &frame) {
+    if(blend_set == false || subfilter == -1 || draw_strings[subfilter] == "ExactImageSubFilter")
+        return;
+    ExactImage(frame);
+    CallFilter(subfilter, frame);
+}
+
+
