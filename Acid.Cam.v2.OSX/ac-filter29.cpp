b0b22fc35800161caa7ab530bda5fe279915290b
Jared Bruni <jaredbruni@gmail.com>
2019-08-28 08:30:05 -0700

    new filter

--- Acid.Cam.v2.OSX/ac-filter29.cpp
+++ Acid.Cam.v2.OSX/ac-filter29.cpp
@@ -419,3 +419,27 @@ void ac::DarkenStrobeMedianBlend(cv::Mat &frame) {
     NoMedianBlurBlendMultiThread(frame);
     AddInvert(frame);
 }
+
+void ac::DarkenImageAlphaBlend(cv::Mat &frame) {
+    if(blend_set == false)
+        return;
+    cv::Mat reimage;
+    ac_resize(blend_image, reimage, frame.size());
+    static double alpha = 1.0;
+    static int dir = 1;
+    auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
+        for(int z = offset; z <  offset+size; ++z) {
+            for(int i = 0; i < cols; ++i) {
+                cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
+                cv::Vec3b pix = reimage.at<cv::Vec3b>(z, i);
+                for(int j = 0; j < 3; ++j) {
+                    pix[j] /= 3;
+                    pixel[j] = ((pixel[j] * alpha) + (pix[j] * (1-alpha)));
+                }
+            }
+        }
+    };
+    UseMultipleThreads(frame, getThreadCount(), callback);
+    AlphaMovementMaxMin(alpha, dir, 0.01, 1.0, 0.1);
+    AddInvert(frame);
+}
--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -1585,6 +1585,7 @@ namespace ac {
     void DarkColors(cv::Mat &frame);
     void DarkenChannelXorNoMedian(cv::Mat &frame);
     void DarkenStrobeMedianBlend(cv::Mat &frame);
+    void DarkenImageAlphaBlend(cv::Mat &frame);
     // #NoFilter
     void NoFilter(cv::Mat &frame);
     // Alpha blend with original image
