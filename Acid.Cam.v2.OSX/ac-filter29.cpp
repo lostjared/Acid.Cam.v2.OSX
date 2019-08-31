ad8eb52181d10f53ba5122196c64578510f0409a
Jared Bruni <jaredbruni@gmail.com>
2019-08-31 05:30:54 -0700

    removed unused variable

--- Acid.Cam.v2.OSX/ac-filter29.cpp
+++ Acid.Cam.v2.OSX/ac-filter29.cpp
@@ -710,13 +710,10 @@ void ac::MonoColorTrails(cv::Mat &frame) {
     frames[0] = collection.frames[1].clone();
     frames[1] = collection.frames[7].clone();
     frames[2] = collection.frames[15].clone();
-    cv::Mat copy1 = frame.clone();
-    CallFilter(subfilter, copy1);
     auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
         for(int z = offset; z <  offset+size; ++z) {
             for(int i = 0; i < cols; ++i) {
                 cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
-                cv::Vec3b pix_copy1 = copy1.at<cv::Vec3b>(z, i);
                 for(int j = 0; j < 3; ++j) {
                     cv::Vec3b pix = frames[j].at<cv::Vec3b>(z, i);
                     pixel[j] = pix[0];
@@ -735,16 +732,13 @@ void ac::TremorMonoColorTrails(cv::Mat &frame) {
     frames[0] = collection.frames[1].clone();
     frames[1] = collection.frames[7].clone();
     frames[2] = collection.frames[15].clone();
-    cv::Mat copy1 = frame.clone();
     static int index = 0;
     int col[3] = {0};
     InitArrayPosition(col, index);
-    CallFilter(subfilter, copy1);
     auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
         for(int z = offset; z <  offset+size; ++z) {
             for(int i = 0; i < cols; ++i) {
                 cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
-                cv::Vec3b pix_copy1 = copy1.at<cv::Vec3b>(z, i);
                 for(int j = 0; j < 3; ++j) {
                     cv::Vec3b pix = frames[j].at<cv::Vec3b>(z, i);
                     pixel[col[j]] = pix[0];
