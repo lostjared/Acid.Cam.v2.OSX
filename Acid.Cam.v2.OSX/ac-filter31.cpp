70dc5635a15d180da8b907cfdb99f1e6bb16a161
Jared Bruni <jaredbruni@gmail.com>
2019-10-03 04:13:49 -0700

    new source file

--- Acid.Cam.v2.OSX/ac-filter31.cpp
+++ Acid.Cam.v2.OSX/ac-filter31.cpp
@@ -1023,30 +1023,3 @@ void ac::RainbowXorStrobeBlend(cv::Mat &frame) {
     flash = (flash == 0) ? 1 : 0;
 }
 
-void ac::IncreaseRainbowXorBlend(cv::Mat &frame) {
-    static MatrixCollection<8> collection;
-    collection.shiftFrames(frame);
-    cv::Mat frames[3];
-    frames[0] = collection.frames[1].clone();
-    frames[1] = collection.frames[4].clone();
-    frames[2] = collection.frames[7].clone();
-    
-    MedianBlendMultiThreadByEight(frame);
-    MedianBlendMultiThread_2160p(frames[1]);
-    MedianBlendIncrease(frames[2]);
-    
-    auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
-        for(int z = offset; z <  offset+size; ++z) {
-            for(int i = 0; i < cols; ++i) {
-                cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
-                for(int j = 0; j <3; ++j) {
-                    cv::Vec3b pix;
-                    pix = frames[j].at<cv::Vec3b>(z, i);
-                    pixel[j] = pixel[j]^pix[j];
-                }
-            }
-        }
-    };
-    UseMultipleThreads(frame, getThreadCount(), callback);
-    AddInvert(frame);
-}
