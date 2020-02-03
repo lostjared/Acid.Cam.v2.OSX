3ed696180eb410fcfff8de69bd7a5def1c7defff
Jared Bruni <jaredbruni@gmail.com>
2020-02-02 16:57:26 -0800

    updated plugin examples

---
+++ plugins/random_pixels/random.cpp
@@ -0,0 +1,11 @@
+#include<opencv2/opencv.hpp>
+
+
+extern "C" void filter(cv::Mat &frame) {
+    for(int z = 0; z < frame.rows; ++z) {
+        for(int i = 0; i < frame.cols; ++i) {
+            frame.at<cv::Vec3b>(z, i) = cv::Vec3b(rand()%255,rand()%255,rand()%255);
+        }
+    }
+}
+
