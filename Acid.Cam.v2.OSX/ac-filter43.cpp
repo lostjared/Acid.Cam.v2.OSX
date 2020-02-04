cc43093126562129ebfc69c37ac0ded46d973e37
Jared Bruni <jaredbruni@gmail.com>
2020-02-04 01:31:54 -0800

    updated plugins Makefiles

--- Acid.Cam.v2.OSX/ac-filter43.cpp
+++ Acid.Cam.v2.OSX/ac-filter43.cpp
@@ -104,7 +104,6 @@ void ac::HorizontalOffsetLess_SubFilter(cv::Mat &frame) {
     static int counter = 0;
     for(int z = 0; z < frame.rows; ++z) {
         for(int i = 0; i < frame.cols; ++i) {
-            
             cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
             if(on == true) {
                 cv::Vec3b pix = frame_copy.at<cv::Vec3b>(z, i);
