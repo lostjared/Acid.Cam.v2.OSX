5d7d1471750f5b062b4fc8696c004236137f735c
Jared Bruni <jaredbruni@gmail.com>
2020-02-13 20:06:21 -0800

    fixed error

--- Acid.Cam.v2.OSX/ac-filter43.cpp
+++ Acid.Cam.v2.OSX/ac-filter43.cpp
@@ -740,7 +740,19 @@ void ac::ZoomCorner(cv::Mat &frame) {
     static double zoom_x = 2, zoom_y = 2;
     cv::Mat copy1;
     cv::resize(frame, copy1, cv::Size(static_cast<int>(frame.cols*zoom_x), static_cast<int>(frame.rows*zoom_y)));
-    frame = copy1(cv::Rect(copy1.cols-frame.cols, copy1.rows-frame.rows, frame.cols, frame.rows));
+    int y = copy1.rows-frame.rows-2;
+    for(int z = 0; z < frame.rows; ++z) {
+        int x = copy1.cols-frame.cols-2;
+        for(int i = 0; i < frame.cols; ++i) {
+            if(x < copy1.cols && y < copy1.rows) {
+                cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
+                cv::Vec3b pix = copy1.at<cv::Vec3b>(y, x);
+                pixel = pix;
+            }
+            ++x;
+        }
+        ++y;
+    }
     static int dir1 = 1, dir2 = 1;
     AlphaMovementMaxMin(zoom_x, dir1, 0.05, 5, 1.0);
     AlphaMovementMaxMin(zoom_y, dir2, 0.05, 5, 1.0);
