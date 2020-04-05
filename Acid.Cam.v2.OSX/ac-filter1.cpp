5e3bb5c9ca38871286a0037adf107d32cbb4162f
Jared Bruni <jaredbruni@gmail.com>
2020-04-04 21:26:36 -0700

    updated versin info

--- Acid.Cam.v2.OSX/ac-filter1.cpp
+++ Acid.Cam.v2.OSX/ac-filter1.cpp
@@ -45,13 +45,13 @@
 // Acid Cam namespace
 namespace ac {
 #if defined(__APPLE__)
-    const std::string version="2.68.0 (macOS)";
+    const std::string version="2.69.0 (macOS)";
 #elif defined(__linux__)
-    const std::string version="2.68.0 (Linux)";
+    const std::string version="2.69.0 (Linux)";
 #elif defined(_WIN32)
-    const std::string version="2.68.0 (Windows)";
+    const std::string version="2.69.0 (Windows)";
 #else
-    const std::string version="2.68.0 (Generic)";
+    const std::string version="2.69.0 (Generic)";
 #endif
     std::mutex col_lock;
     bool swapColorOn = true;
