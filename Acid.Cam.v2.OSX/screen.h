63fe2b39426e9ce395152c6bba40caa98de25653
Jared Bruni <jaredbruni@gmail.com>
2020-04-22 18:08:07 -0700

    added guards

--- Acid.Cam.v2.OSX/screen.h
+++ Acid.Cam.v2.OSX/screen.h
@@ -1,10 +1,13 @@
-
+#ifndef __SCREEN_H__
+#define __SCREEN_H__
+#ifdef __APPLE__
 #import <AppKit/AppKit.h>
 #include "ac.h"
-
 @interface NSImage (NSImage_OpenCV) {
     
 }
 @property(nonatomic, readonly) cv::Mat CVMat;
 @end
+#endif
+#endif
 
