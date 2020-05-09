1474e51792ae7b90d4879adf5d412e200711f304
Jared Bruni <jaredbruni@gmail.com>
2020-05-08 23:10:11 -0700

    updated

--- Acid.Cam.v2.OSX/AC_AppDelegate.mm
+++ Acid.Cam.v2.OSX/AC_AppDelegate.mm
@@ -167,13 +167,15 @@
                                                                        // ...then update the view and mark it as needing display
                                                                        self.image_view.image = frame;
                                                                        cv::Mat img;
-                                                                       img.create(imageSize.height,imageSize.width,CV_8UC3);
+                                                                       img.create(imageSize.height,imageSize.width,CV_8UC4);
                                                                        glPixelStorei(GL_PACK_ALIGNMENT, (img.step & 3) ? 1 : 4);
                                                                        glPixelStorei(GL_PACK_ROW_LENGTH, (GLint)img.step/img.elemSize());
-                                                                       glReadPixels(0, 0, img.cols, img.rows, GL_BGR, GL_UNSIGNED_BYTE, img.data);
+                                                                       glReadPixels(0, 0, img.cols, img.rows, GL_BGRA, GL_UNSIGNED_BYTE, img.data);
                                                                        cv::Mat flipped;
                                                                        cv::flip(img, flipped, 0);
-                                                                       ac::setNewSyphonImage(flipped);
+                                                                       cv::cvtColor(flipped, img, CV_BGRA2BGR);
+                                                                       ac::setNewSyphonImage(img);
+                                                                       
                                                                        [self.image_view setNeedsDisplay:YES];
                                                                    }];
                                                                }];
