6a4c82bce0db110b6e70aa32379607ba119d5dc6
Jared Bruni <jaredbruni@gmail.com>
2020-05-10 15:07:48 -0700

    Updated timer timeout

--- Acid.Cam.v2.OSX/AC_Controller.mm
+++ Acid.Cam.v2.OSX/AC_Controller.mm
@@ -897,10 +897,10 @@ void setEnabledProg() {
             ret_val = program_main(outputVideo, value_w, value_h, syphon_enabled, set_frame_rate, set_frame_rate_val, u4k, (int)popupType, input_file, r, filename, (int)cap_width, (int)cap_height,(int)[device_index indexOfSelectedItem], 0, 0.75f, add_path);
         if(ret_val == 0) {
             if(camera_mode == 1) {
-                renderTimer = [NSTimer timerWithTimeInterval:1.0/ac::fps target:self selector:@selector(cvProc:) userInfo:nil repeats:YES];
+                renderTimer = [NSTimer timerWithTimeInterval:1.0/1000 target:self selector:@selector(cvProc:) userInfo:nil repeats:YES];
             }
             else {
-                renderTimer = [NSTimer timerWithTimeInterval:1.0/ac::fps target:self selector:@selector(camProc:) userInfo:nil repeats:YES];
+                renderTimer = [NSTimer timerWithTimeInterval:1.0/1000 target:self selector:@selector(camProc:) userInfo:nil repeats:YES];
                 [[NSRunLoop currentRunLoop] addTimer:renderTimer forMode:NSEventTrackingRunLoopMode];
             }
             [[NSRunLoop currentRunLoop] addTimer:renderTimer forMode:NSDefaultRunLoopMode];
