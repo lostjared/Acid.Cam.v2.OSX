4012a0f6ca7687e64efa0fdbadc049c349b7cda3
Jared Bruni <jaredbruni@gmail.com>
2020-05-29 03:37:20 -0700

    updated

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -3339,22 +3339,28 @@ namespace ac {
         int r = 3+rand()%7;
         for(int i = 0; i < r; ++i)
             MedianBlur(frame);
+
         collection->shiftFrames(frame);
+        
         static int position = 2;
         static auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
             for(int z = offset; z <  offset+size; ++z) {
                 for(int i = 0; i < cols; ++i) {
-                    cv::Scalar value;
-                    for(int j = 0; j < position; ++j) {
-                        cv::Vec3b pixel = collection->frames[j].template at<cv::Vec3b>(z, i);
-                        for(int q = 0; q < 3; ++q) {
-                            value[q] += pixel[q];
+                    if(i >= 0 && i < frame->cols && z >= 0 && z < frame->rows) {
+                        cv::Scalar value;
+                        for(int j = 0; j < position; ++j) {
+                            if(j >= 0 && j < 3) {
+                                cv::Vec3b pixel = collection->frames[j].template at<cv::Vec3b>(z, i);
+                                for(int q = 0; q < 3; ++q) {
+                                    value[q] += pixel[q];
+                                }
+                            }
                         }
-                    }
-                    cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
-                    for(int j = 0; j < 3; ++j) {
-                        int val = 1+static_cast<int>(value[j]);
-                        pixel[j] = static_cast<unsigned char>(pixel[j] ^ val);
+                        cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
+                        for(int j = 0; j < 3; ++j) {
+                            int val = 1+static_cast<int>(value[j]);
+                            pixel[j] = static_cast<unsigned char>(pixel[j] ^ val);
+                        }
                     }
                 }
             }
