ec83efdd4943261e720c51ad4f41b84d37769e1e
Jared Bruni <jaredbruni@gmail.com>
2020-06-04 04:43:18 -0700

    fixed bug

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -3346,25 +3346,22 @@ namespace ac {
     
     template<int SIZE>
     void MedianBlendMultiThreadVariable(cv::Mat &frame, MatrixCollection<SIZE> *collection) {
-        int r = 3+rand()%7;
-        for(int i = 0; i < r; ++i)
-            MedianBlur(frame);
         collection->shiftFrames(frame);
         static int position = 2;
-        static auto callback = [&](cv::Mat *frame, int offset, int cols, int size) {
-            for(int z = offset; z <  offset+size; ++z) {
-                for(int i = 0; i < cols; ++i) {
-                    if(i >= 0 && i < frame->cols && z >= 0 && z < frame->rows) {
-                        cv::Scalar value;
-                        for(int j = 0; j < position; ++j) {
-                            if(j >= 0 && j < 3) {
-                                cv::Vec3b pixel = collection->frames[j].template at<cv::Vec3b>(z, i);
-                                for(int q = 0; q < 3; ++q) {
-                                    value[q] += pixel[q];
-                                }
+        for(int z = 0; z <  frame.rows; ++z) {
+            for(int i = 0; i < frame.cols; ++i) {
+                if(i >= 0 && i < frame.cols && z >= 0 && z < frame.rows) {
+                    cv::Scalar value;
+                    for(int j = 0; j < 3; ++j) {
+                        if(j >= 0 && j < 3 && i >= 0 && i < collection->frames[j].cols && z >= 0 && z < collection->frames[j].rows) {
+                            cv::Vec3b pixel = collection->frames[j].template at<cv::Vec3b>(z, i);
+                            for(int q = 0; q < 3; ++q) {
+                                value[q] += pixel[q];
                             }
                         }
-                        cv::Vec3b &pixel = frame->at<cv::Vec3b>(z, i);
+                    }
+                    if(i >= 0 && i < frame.cols && z >= 0 && z < frame.rows) {
+                        cv::Vec3b &pixel = frame.at<cv::Vec3b>(z, i);
                         for(int j = 0; j < 3; ++j) {
                             int val = 1+static_cast<int>(value[j]);
                             pixel[j] = static_cast<unsigned char>(pixel[j] ^ val);
@@ -3372,9 +3369,7 @@ namespace ac {
                     }
                 }
             }
-        };
-        UseMultipleThreads(frame, getThreadCount(), callback);
-        AddInvert(frame);
+        }
         static int dir = 1;
         if(dir == 1) {
             ++position;
