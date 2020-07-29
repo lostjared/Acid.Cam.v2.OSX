5bc18cc1cb59e599305a9fc17d577e4cd5cb64cf
Jared Bruni <jaredbruni@gmail.com>
2020-07-29 15:40:00 -0700

    new filter

--- Acid.Cam.v2.OSX/ac.h
+++ Acid.Cam.v2.OSX/ac.h
@@ -2448,6 +2448,7 @@ namespace ac {
     void LineInLineOut_Increase(cv::Mat &frame);
     void LineInLineOut2_Increase(cv::Mat &frame);
     void LineInLineOut3_Increase(cv::Mat &frame);
+    void LineInLineOut4_Increase(cv::Mat &frame);
     // #NoFilter
     void NoFilter(cv::Mat &frame);
     void Empty(cv::Mat &frame);
